cf990b36deda1ddde14a8e778116a2b667179ab8
Vicma98 <vhernandez@unac.edu.co>
2025-11-07 12:43:28 -0500

    2commit

--- lib/widgets/weather_service.dart
+++ lib/widgets/weather_service.dart
@@ -4,10 +4,17 @@ import 'package:http/http.dart' as http;
 class WeatherService {
   // Retorna temperatura en Celsius como double
   Future<double> fetchTemperature(double lat, double lon) async {
-    final url = Uri.parse('https://api.open-meteo.com/v1/forecast'
+    final url = Uri.parse('https://api.open-meteo.com/v1/forecast?latitude=6.2447&longitude=-75.5748&current=temperature'
         '?latitude=$lat&longitude=$lon&current=temperature');
-    final response = await http.get(url).timeout(const Duration(seconds: 8));
-    if (response.statusCode == 200) {
+        final response = await http.get(Uri.parse('https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&current=temperature'
+        ));
+
+    print('Status code: ${response.statusCode}');
+    print('Body: ${response.body}');
+    
+
+
+if (response.statusCode == 200) {
       final Map<String, dynamic> json = jsonDecode(response.body);
       if (json.containsKey('current') && json['current'] != null) {
         // La API devuelve current.temperature
